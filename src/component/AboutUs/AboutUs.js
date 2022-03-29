7d6d475fa187893dd817bab807bd698711c35732
Md. Ibrahim Hossain <ih75754@gmail.com>
2022-03-29 11:56:34 +0600

    about us page

--- src/component/AboutUs/AboutUs.js
+++ src/component/AboutUs/AboutUs.js
@@ -8,8 +8,8 @@ const AboutUs = () => {
             <Header></Header>
             <div className="container py-5">
                 <div className="text-center pb-5">
-                    <h1>ABOUT PureSmile</h1>
-                    <p>PureSmile PROFILE & HISTORY</p>
+                    <h1>ABOUT Ed-Tech</h1>
+                    <p>Ed-Tech PROFILE & HISTORY</p>
                 </div>
                 <div className="row">
                     <div className="col-md-4">
@@ -25,7 +25,7 @@ const AboutUs = () => {
                         </div>
                     </div>
                     <div className="col-md-4">
-                        <img src="https://image.freepik.com/free-photo/portrait-smiling-handsome-male-doctor-man_171337-5055.jpg" className="img-fluid h-100" alt="" />
+                        <img src="http://themes.g5plus.net/academia/wp-content/uploads/2016/01/bg-icon-box-2.jpg" className="img-fluid h-100" alt="" />
                     </div>
                     <div className="col-md-4">
                         <div className="pb-5">
